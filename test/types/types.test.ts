62b3f895d0f1a415bc0b262f2f20021f782f4827
climba03003 <kaka@kakawebsitedemo.com>
2020-10-10 16:17:13 +0800

    feat: add typing (#14)
    * feat: :label: add typing
    
    * feat: :label: better type
    
    envSchemaData
    
    * test: :white_check_mark: better type checking

---
+++ test/types/types.test.ts
@@ -0,0 +1,55 @@
+import { expectError, expectType } from "tsd";
+import envSchema, { EnvSchemaData, EnvSchemaOpt } from "../..";
+
+const schema = {
+  type: "object",
+  required: ["PORT"],
+  properties: {
+    PORT: {
+      type: "string",
+      default: 3000,
+    },
+  },
+};
+const data = {
+  foo: "bar",
+};
+
+expectType<EnvSchemaData>(envSchema());
+
+const emptyOpt: EnvSchemaOpt = {};
+expectType<EnvSchemaOpt>(emptyOpt);
+
+const optWithSchema: EnvSchemaOpt = {
+  schema,
+};
+expectType<EnvSchemaOpt>(optWithSchema);
+
+const optWithData: EnvSchemaOpt = {
+  data,
+};
+expectType<EnvSchemaOpt>(optWithData);
+
+expectError<EnvSchemaOpt>({
+  data: [], // min 1 item
+});
+
+const optWithArrayData: EnvSchemaOpt = {
+  data: [{}],
+};
+expectType<EnvSchemaOpt>(optWithArrayData);
+
+const optWithMultipleItemArrayData: EnvSchemaOpt = {
+  data: [{}, {}],
+};
+expectType<EnvSchemaOpt>(optWithMultipleItemArrayData);
+
+const optWithDotEnvBoolean: EnvSchemaOpt = {
+  dotenv: true,
+};
+expectType<EnvSchemaOpt>(optWithDotEnvBoolean);
+
+const optWithDotEnvOpt: EnvSchemaOpt = {
+  dotenv: true,
+};
+expectType<EnvSchemaOpt>(optWithDotEnvOpt);
